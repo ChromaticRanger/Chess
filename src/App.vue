97e6174090e0cbd777165b56a1e828dda3dd2554
Martin Stickley <mds1966@gmail.com>
2025-03-16 12:56:08 +0000

    Add a decorative border around the board component for improved visual appeal

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,17 @@ import Board from "./components/Board.vue";
 
 <template>
   <div class="flex items-center justify-center min-h-screen">
-    <Board />
+    <div class="border-brown border-10 rounded">
+      <Board />
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.border-brown {
+  border: 10px solid #8B4513; /* SaddleBrown color */
+}
+.border-10 {
+  border-width: 10px;
+}
 </style>
